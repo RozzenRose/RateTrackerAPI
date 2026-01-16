0b328ca2a861ebb56716907a8ee9b63b5d969a77
RozzenRose <RozzenRose@gmail.com>
2026-01-16 15:34:33 +0100

    docker compose implemented

--- app/database/models/eth_history.py
+++ app/database/models/eth_history.py
@@ -5,5 +5,5 @@ from sqlalchemy import Column, func, DateTime, Float
 class EthUsd(Base):
     __tablename__ = 'eth_usd'
 
-    date_time = Column(DateTime, server_default=func.now(), primary_key=True)
+    date_time = Column(DateTime(timezone=True), server_default=func.current_timestamp(), primary_key=True)
     rate = Column(Float)
